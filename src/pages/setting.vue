26e2a1c672069f5a9b86cd16b915b8274d50587a
angel bests <angelbests1314@live.com>
2024-11-19 02:47:10 +0800

    fix bug

--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -150,7 +150,7 @@ const refresh = function(){
                     <!-- <v-list-item prepend-icon="mdi-note-outline" title="剪贴板" :href="''"></v-list-item> -->
                     <!-- <v-list-item prepend-icon="mdi-calendar-range" title="calendar" :href="'/#/pages/setting/calendar'"></v-list-item> -->
                     <v-list-item prepend-icon="mdi-calendar-range" title="日历" :href="'/#/pages/setting/datenote'"></v-list-item>
-                    <v-list-item prepend-icon="mdi-note-outline" title="系统" :href="'/#/pages/setting/system'"></v-list-item>
+                    <!-- <v-list-item prepend-icon="mdi-note-outline" title="系统" :href="'/#/pages/setting/system'"></v-list-item> -->
                 </v-list>
             </v-navigation-drawer>
             <router-view v-slot="{ Component }" :key="$route.fullPath" style="width: auto;height: 100%;box-sizing: border-box;padding: 10px;">
