4a226b8e1260dd6803bc6861856f1f406e147ffe
angelbests <angelbests1314@live.com>
2025-05-03 20:58:58 +0800

    Update MusicTape.vue

--- src/components/MusicTape.vue
+++ src/components/MusicTape.vue
@@ -4,18 +4,7 @@
 
 <template>
     <div id="music_tape" class="music_tape">
-        <div style="width: 50px;height: 50px;border-radius: 50%;background: red;">
-
-        </div>
     </div>
 </template>
 
-<style>
-.music_tape {
-    position: absolute;
-    width: 400px;
-    height: 220px;
-    background: white;
-    border-radius: 10px;
-}
-</style>
+<style></style>
